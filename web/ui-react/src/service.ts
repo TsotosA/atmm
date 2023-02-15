bac75155bdc00a07f46014743d6b3a78725e037d
TsotosA <TsotosA@users.noreply.github.com>
2023-02-15 12:20:57 +0200

    .

--- web/ui-react/src/service.ts
+++ web/ui-react/src/service.ts
@@ -1,10 +1,10 @@
 export const fetchConfiguration = async () => {
-    const res = await fetch('http://localhost:1323/api/config')
+    const res = await fetch(`${process.env.REACT_APP_API_ROOT}/api/config`)
     return res.json()
 }
 
 export const saveConfiguration = async (conf: any) => {
-    const res = await fetch('http://localhost:1323/api/config', {
+    const res = await fetch(`${process.env.REACT_APP_API_ROOT}/api/config`, {
         method: "POST",
         headers: {
             'Content-Type': 'application/json'
@@ -15,6 +15,6 @@ export const saveConfiguration = async (conf: any) => {
 }
 
 export const fetchLogs = async (nItems: number, grepFor?: string) => {
-    const res = await fetch(`http://localhost:1323/api/log?nItems=${nItems}&grepFor=${grepFor}`)
+    const res = await fetch(`${process.env.REACT_APP_API_ROOT}/api/log?nItems=${nItems}&grepFor=${grepFor}`)
     return await res.text();
 }
